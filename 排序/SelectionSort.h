af2284033f05ade1e15199d943b9f144be7d47c5
weiison <42989673+weiison@users.noreply.github.com>
2020-03-12 09:46:41 +0800

    Update SelectionSort.h

--- 排序/SelectionSort.h
+++ 排序/SelectionSort.h
@@ -1,2 +1,15 @@
-void selectionSort
-		
+void selectionSort(vector<int>& v)
+{
+	int size=v.size();
+	for(int i=0;i<size;++i)
+	{
+		int min=i;
+		for(int j=i+1;j<size;++j)
+		{
+			if(v[j]<v[min])
+				min=j;
+		}
+		if(min!=i)
+			swap(v[min],v[i]);
+	}
+}
