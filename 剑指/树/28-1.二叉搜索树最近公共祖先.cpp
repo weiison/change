1b9757b7a0c12b49cbc89c9a3c68e54f9b636027
weiison <42989673+weiison@users.noreply.github.com>
2020-03-14 15:44:39 +0800

    Create 28-1.二叉搜索树最近公共祖先.cpp

---
+++ 剑指/树/28-1.二叉搜索树最近公共祖先.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    //利用搜索树特点判断
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if((p->val<=root->val && q->val>=root->val)||(p->val>=root->val && q->val<=root->val))
+            return root;
+        if(p->val<root->val&&q->val<root->val)
+            return lowestCommonAncestor(root->left,p,q);
+        else
+            return lowestCommonAncestor(root->right,p,q);      
+    }
+};
