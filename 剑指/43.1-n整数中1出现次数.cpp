a53a3c59c6d3d6c5a92be74459b547ab7cf51c4c
weiison <42989673+weiison@users.noreply.github.com>
2020-03-17 14:43:26 +0800

    Update 43.1-n整数中1出现次数.cpp

--- 剑指/43.1-n整数中1出现次数.cpp
+++ 剑指/43.1-n整数中1出现次数.cpp
@@ -14,7 +14,7 @@ public:
         int len=s.size()-begin;
         if(len==1&&first==0) return 0;
         if(len==1&&first>0) return 1;
-        int numFirstDigit=0;//计算首位是1的所有次数
+        int numFirstDigit=0;//计算首位是1的所有次数,对于11只算了一次
         if(first>1)
             numFirstDigit=powBase10(len-1);
         else if(first==1)//首位是0不计算！！如1080的子串，去掉1后为080
@@ -23,7 +23,7 @@ public:
             numFirstDigit=stoi(temp)+1;
         }
         int numOther=first*(len-1)*powBase10(len-2);//首位不是1其他位是1的次数
-                                                   //首位大小（第一位次数）*剩下位数选一位是1*其余位数出现的排列组合
+         //首位数字（0-9中除了1之外的个数）*剩下位数选一位是1*其余位数出现的排列组合（11的另一次在这里计算）
         int numSub=numOf1(s,begin+1);//递归处理去掉首位的子字符串
         return numFirstDigit+numOther+numSub;
     }
