7b726b8dce3b67aa964b0cc0dbf1306ce2fb379a
weiison <42989673+weiison@users.noreply.github.com>
2020-04-17 11:44:50 +0800

    Create (记忆化回溯)140.单词拆分.cpp

---
+++ LC/(记忆化回溯)140.单词拆分.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> dfs(string& s,int cur,unordered_set<string>& st,unordered_map<int,vector<string>>& mp)
+    {
+        if(mp.find(cur)!=mp.end())
+        {
+            return mp[cur];
+        }
+        vector<string> res;
+        if(cur==s.size())
+            res.push_back("");
+        else
+        {
+            for(int i=cur;i<s.size();++i)
+            {
+                string strtmp=s.substr(cur,i-cur+1);
+                if(st.find(strtmp)!=st.end())
+                {
+                    vector<string> tmp=dfs(s,i+1,st,mp);//获得之后的拆分结果
+                    for(int i=0;i<tmp.size();++i)
+                    {
+                        res.push_back(strtmp+(tmp[i].empty()?"":" ")+tmp[i]);
+                    }
+                }
+            }
+        }
+        mp[cur]=res;
+        return res;
+    }
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        vector<string> res;
+        unordered_set<string> st(wordDict.begin(),wordDict.end());
+        unordered_map<int,vector<string>> mp;//当前下标位置的结果
+        res=dfs(s,0,st,mp);
+        return res;
+    }
+};
